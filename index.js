6c714eaf6b5e7658a0f409e82af0e3be5bc36b6e
subhashprasad2001@gmail.com <96110162+subhashdippu@users.noreply.github.com>
2024-06-16 21:18:16 +0530

    First Commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 6001;
+const port = process.env.PORT || 10000;
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
